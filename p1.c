116e0c0a775d1e21fa09417be9a7c7319bad3bfc
Andrew Greenman <greenman@mail.gvsu.edu>
2014-10-01 13:10:22 -0400

    Finished Process 0 and Process 1.

--- p1.c
+++ p1.c
@@ -27,11 +27,15 @@ int main(int argc, char **argv)
 
   pid_t pid;
   FILE *vector_a, *vector_b;
-  int pipe1_fd[2], pipe2_fd[2];
+  int pipe1_fd[2], pipe2_fd[2], decimal_value;
   char input_a[MAX_VECTOR_SIZE], input_b[MAX_VECTOR_SIZE];
 
   vector_a = fopen(argv[1], "r");
   vector_b = fopen(argv[2], "r");
+
+  // Create pipes.
+  pipe(pipe1_fd);
+  pipe(pipe2_fd);
 
   pid = fork();
   if(pid < 0) {
@@ -45,17 +49,45 @@ int main(int argc, char **argv)
     } else if(pid) {
       // Process 2
 
+      close(pipe1_fd[PIPE_READ]); // Close the read portion of pipe 1.
+      close(pipe1_fd[PIPE_WRITE]); // Close the write portion of pipe 1.
+      close(pipe2_fd[PIPE_WRITE]); // Close the write portion of pipe 2.
+
       while(fgets(input_a, MAX_VECTOR_SIZE, vector_a) != NULL) {
-        // printf("Parent Vector A: %s\n", input_a);
+        // Reads in and process input_a line by line.
+
+        read(pipe2_fd[PIPE_READ], input_b, sizeof(input_b)); // Reads in the two's complimented input_b from the pipe.
       }
+
+      close(pipe2_fd[PIPE_READ]); // Close the read portion of pipe 2.
     } else {
       // Process 1
 
-      dup2(pipe1_fd[PIPE_READ], STDIN_FILENO);
-      convert_binary_to_decimal(input_b);
+      close(pipe1_fd[PIPE_WRITE]); // Close the write portion of pipe 1.
+      close(pipe2_fd[PIPE_READ]); // Close the read portion of pipe 2.
+
+      while(read(pipe1_fd[PIPE_READ], input_b, sizeof(input_b))) {
+        decimal_value = convert_binary_to_decimal(input_b); // Convert the binary number to decimal for easier math.
+        decimal_value++; // Increments the decimal value by 1.
+
+        sprintf(input_b, "%d", decimal_value);
+
+        write(pipe2_fd[PIPE_WRITE], (const void *) input_b, (size_t) strlen(input_b) + 1);
+
+        sleep(1);
+      }
+
+      close(pipe1_fd[PIPE_READ]); // Close the read portion of pipe 1.
+      close(pipe2_fd[PIPE_WRITE]); // Close the write portion of pipe 2.
+
+      exit(0);
     }
   } else {
     // Process 0
+
+    close(pipe1_fd[PIPE_READ]); // Close the read portion of pipe 1.
+    close(pipe2_fd[PIPE_READ]); // Close the read portion of pipe 2.
+    close(pipe2_fd[PIPE_WRITE]); // Close the write portion of pipe 2.
 
     while(fgets(input_b, MAX_VECTOR_SIZE, vector_b) != NULL) {
       // Reads in and processes input_b line by line.
@@ -67,11 +99,13 @@ int main(int argc, char **argv)
 
       complementer(input_b); // Bitwise complement on the binary number.
 
-      dup2(pipe1_fd[PIPE_WRITE], STDOUT_FILENO); // Set up a pipe to write to.
-      write(STDOUT_FILENO, (const void *) input_b, (size_t) strlen(input_b) + 1);
-      close(pipe1_fd[PIPE_READ]);
-      close(pipe1_fd[PIPE_WRITE]);
+      // Write the complemented number to the pipe.
+      write(pipe1_fd[PIPE_WRITE], (const void *) input_b, (size_t) strlen(input_b) + 1);
+
+      sleep(1);
     }
+
+    close(pipe1_fd[PIPE_WRITE]); // Close the write portion of pipe 1.
 
     exit(0);
   }
@@ -84,8 +118,9 @@ int main(int argc, char **argv)
  * integer form. The placeholder variable holds each bit in integer form. *
  * The multiplier variables is used to multiply the placeholder by the    *
  * correct power of two. The sum contains the running sum of the binary   *
- * number. The final integer representation of the number, held by the    *
- * variable sum, is returned.                                             *
+ * number. Since the value is in two's compliment, a special step is      *
+ * taken to convert to a negative number. The final integer               *
+ * representation of the number, held by the variable sum, is returned.   *
  **************************************************************************/
 int convert_binary_to_decimal(char *binary_string)
 {
@@ -95,6 +130,19 @@ int convert_binary_to_decimal(char *binary_string)
     placeholder = binary_string[i] - '0';
     sum += placeholder * (1 << multiplier++);
   }
+
+  if(binary_string[0] == '1') {
+    int diff = 1;
+
+    for(int p = 1; p <= length; p++) {
+      diff *= 2;
+    }
+
+    sum -= diff;
+  }
+
+  printf("%s = %d\n", binary_string, sum);
+
 
   return sum;
 }
